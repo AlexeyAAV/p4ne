d940a6e67f1dfca99a366b2da4b013c7a15e96e4
AlexeyAAV <snegomail@gmail.com>
2017-06-09 18:38:13 +0300

    for Max

--- Lab1.5/1.6 reg exp.py
+++ Lab1.5/1.6 reg exp.py
@@ -1,12 +1,17 @@
 import glob
 import re
 flist=glob.iglob(r"C:\Users\Anastasiev\Desktop\Seafile libs\Seafile\p4ne_training\config_files\*.txt")
-l = []
+ip = []
+int = []
 for i in flist:
     with open(i) as f:
         for line in f:
             if re.match('^(.+?)ip address(.+?)$', line):
-                l.append(line.strip())
+                ip.append(line.strip())
+            if re.match('^(.+?)INT(.+?)$', line):
+                int.append(line.strip())
 
-l  = list(set(l))
-print(l)
+ip  = list(set(ip))
+print(ip)
+int  = list(set(int))
+print(int)
